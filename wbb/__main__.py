4ac6d0d819f02991bd04e6093e92bb8dc48b50b5
jackryder6 <86863569+jackryder6@users.noreply.github.com>
2021-11-01 13:32:18 +0530

    Update __main__.py

--- wbb/__main__.py
+++ wbb/__main__.py
@@ -109,7 +109,7 @@ home_keyboard_pm = InlineKeyboardMarkup(
             ),
             InlineKeyboardButton(
                 text="Repo 🛠",
-                url="https://github.com/thehamkercat/WilliamButcherBot",
+                url="https://telegra.ph/file/d81e76bb758db593f4365.mp4",
             ),
         ],
         [
@@ -118,7 +118,7 @@ home_keyboard_pm = InlineKeyboardMarkup(
                 callback_data="stats_callback",
             ),
             InlineKeyboardButton(
-                text="Support 👨", url="http://t.me/WBBSupport"
+                text="Support 👨", url="https://telegra.ph/file/d81e76bb758db593f4365.mp4"
             ),
         ],
         [
@@ -146,7 +146,7 @@ keyboard = InlineKeyboardMarkup(
             ),
             InlineKeyboardButton(
                 text="Repo 🛠",
-                url="https://github.com/thehamkercat/WilliamButcherBot",
+                url="https://telegra.ph/file/d81e76bb758db593f4365.mp4",
             ),
         ],
         [
@@ -154,7 +154,7 @@ keyboard = InlineKeyboardMarkup(
                 text="System Stats 💻",
                 callback_data="stats_callback",
             ),
-            InlineKeyboardButton(text="Support 👨", url="t.me/WBBSupport"),
+            InlineKeyboardButton(text="Support 👨", url="https://telegra.ph/file/d81e76bb758db593f4365.mp4"),
         ],
     ]
 )
